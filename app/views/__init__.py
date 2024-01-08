031b5b873c31e2327d6c5dfffe504385270776d8
Zias-Git <jeremy.adam@gmx.de>
2024-01-08 20:10:22 +0100

    added link to sign up button in header, now links to singup page submit button now calls submit function which does nothing atm

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,6 +3,7 @@ from app.views import startPage
 from app.views import profileView
 from app.views import signUpView
 from app.views import editProfileView
+from app.services import signUpService
 from flask import Blueprint
 from markupsafe import escape
 # from app.components import errorDialog as e
@@ -23,6 +24,10 @@ def profile(uid: int):
 def signUp():
     #TODO:
     return signUpView.SignUpView.loadPage()
+
+@blueprint.route('/submit')
+def submit():
+    return signUpService.SignUpService.submit()
 
 @blueprint.route('/editProfile')
 def editProfile():
