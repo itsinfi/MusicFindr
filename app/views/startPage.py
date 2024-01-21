a16d7d55b6362d94ecde127d435900dba5d343d5
tijanab256 <tijana.belau@iu-study.org>
2024-01-21 13:39:29 +0100

    anpassungen discoverarea, anklickbare tags

--- app/views/startPage.py
+++ app/views/startPage.py
@@ -8,6 +8,15 @@ class StartPage:
     @staticmethod
     def getDiscoverarea():
         from app.services.playlistService import PlaylistService as p
+        from app.services import tagService as t
         
         shuffled_playlist = p.getShuffledPlaylist()
-        return render_template('content/start.html', shuffled_playlist=shuffled_playlist)
+        tagTitles = {}
+        for playlist in shuffled_playlist:
+            playlistTagsTitles = []
+            for tagID in playlist.tags:
+                tag = t.TagService.readTag(tagID)
+                playlistTagsTitles.append(tag.title)
+            tagTitles[playlist.id] = playlistTagsTitles 
+        return render_template('content/start.html', shuffled_playlist=shuffled_playlist, playlists=tagTitles)
+ 
