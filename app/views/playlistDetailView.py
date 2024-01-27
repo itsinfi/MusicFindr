f7a7000786c18b6afc67a6563359a753705c4ddd
itsinfi <sascha.angermann2002@gmail.com>
2024-01-28 00:00:11 +0100

    add spotify and soundcloud embedded links to playlist detail view

--- app/views/playlistDetailView.py
+++ app/views/playlistDetailView.py
@@ -31,5 +31,10 @@ class PlaylistDetailView():
 
         except  playlistService.PlaylistServiceError as e:
             raise e
+        
+        embeddedLink = False
+        if (playlist.platform == "spotify"):
+            playlistID = playlist.link.split('/')[-1].split('?')[0]
+            embeddedLink = f"https://open.spotify.com/embed/playlist/{playlistID}"
 
-        return render_template('content/playlistDetail.html', pid = playlist.id, title = playlist.title, description = playlist.description, link = playlist.link, platform = playlist.platform, tags = sorted(tags, key = voteService.VoteService.getVoteNumberOnPlaylistTag, reverse = True), votes = votes, loggedin=userService.UserService.checkCurrentUserIsLoggedIn())
+        return render_template('content/playlistDetail.html', pid = playlist.id, title = playlist.title, description = playlist.description, link = playlist.link, platform = playlist.platform, embeddedLink = embeddedLink, tags = sorted(tags, key = voteService.VoteService.getVoteNumberOnPlaylistTag, reverse = True), votes = votes, loggedin=userService.UserService.checkCurrentUserIsLoggedIn())
